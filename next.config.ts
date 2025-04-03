1f35740776fa8dcf9de5b0d455775fce9c47c5c9
Bernardo Ragonezi <bernardo_ragonezi@gotmail.com>
2025-04-03 00:14:04 -0300

    publish middle versioon

---
+++ next.config.ts
@@ -0,0 +1,23 @@
+// const isProd = process.env.NODE_ENV === "production";
+
+// const nextConfig = {
+//   reactStrictMode: true,
+//   output: "export", // Enables static export
+//   basePath: isProd ? "/Ragonezi96.github.io" : "", // Set base path for GitHub Pages
+//   assetPrefix: isProd ? "/Ragonezi96.github.io/" : "", // Set asset prefix for GitHub Pages
+// };
+
+// export default nextConfig;
+const isProd = process.env.NODE_ENV === "production";
+
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export", // Enables static export
+  basePath: isProd ? "/Ragonezi96.github.io" : "", // Base path for GitHub Pages
+  assetPrefix: isProd ? "/Ragonezi96.github.io/" : "", // Asset prefix for GitHub Pages
+  images: {
+    unoptimized: true, // Disable image optimization for static export
+  },
+};
+
+export default nextConfig;
