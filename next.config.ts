67deeb87a8e526fc9642804995edc737466dc09d
Bernardo Ragonezi <bernardo_ragonezi@gotmail.com>
2025-04-03 01:00:24 -0300

    update config

--- next.config.ts
+++ next.config.ts
@@ -1,32 +1,10 @@
-// const isProd = process.env.NODE_ENV === "production";
-
-// const nextConfig = {
-//   reactStrictMode: true,
-//   output: "export", // Enables static export
-//   basePath: isProd ? "/Ragonezi96.github.io" : "", // Set base path for GitHub Pages
-//   assetPrefix: isProd ? "/Ragonezi96.github.io/" : "", // Set asset prefix for GitHub Pages
-// };
-
-// export default nextConfig;
-const isProd = process.env.NODE_ENV === "production";
-
 const nextConfig = {
   reactStrictMode: true,
-  output: "export", // Enables static export
-  // basePath: isProd ? "/Ragonezi96.github.io" : "", // Base path for GitHub Pages
-  // assetPrefix: isProd ? "/Ragonezi96.github.io/" : "", // Asset prefix for GitHub Pages
+  output: "export",
+  assetPrefix: "/", // important for GitHub Pages
   images: {
-    unoptimized: true, // Disable image optimization for static export
+    unoptimized: true,
   },
 };
 
 export default nextConfig;
-
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   output: 'export',
-//   basePath: '/my-portfolio',
-//   assetPrefix: '/my-portfolio',
-// };
-
-// export default nextConfig;
