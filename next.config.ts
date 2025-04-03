624086a368a7bd724cbd34fe28e18296feed2b38
Bernardo Ragonezi <bernardo_ragonezi@gotmail.com>
2025-04-03 00:43:51 -0300

    fix local static

--- next.config.ts
+++ next.config.ts
@@ -13,11 +13,20 @@ const isProd = process.env.NODE_ENV === "production";
 const nextConfig = {
   reactStrictMode: true,
   output: "export", // Enables static export
-  basePath: isProd ? "/Ragonezi96.github.io" : "", // Base path for GitHub Pages
-  assetPrefix: isProd ? "/Ragonezi96.github.io/" : "", // Asset prefix for GitHub Pages
+  // basePath: isProd ? "/Ragonezi96.github.io" : "", // Base path for GitHub Pages
+  // assetPrefix: isProd ? "/Ragonezi96.github.io/" : "", // Asset prefix for GitHub Pages
   images: {
     unoptimized: true, // Disable image optimization for static export
   },
 };
 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   output: 'export',
+//   basePath: '/my-portfolio',
+//   assetPrefix: '/my-portfolio',
+// };
+
+// export default nextConfig;
